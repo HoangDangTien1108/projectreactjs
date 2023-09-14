bcd79ad34629f2f967109d74ea19e9b27ec786b5
tiendp <hoangdangtien1108@gmail.com>
2023-09-14 13:19:41 +0700

    upload code

---
+++ src/Form.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function Form(props){
+    const {a, b, hanldeClick} = props;
+    return (
+    <>
+        <div class="card">
+            <input type="text" />
+        </div>
+    </>
+    )
+}
+
+export default Form 
